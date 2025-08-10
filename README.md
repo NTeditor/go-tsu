80c70616bf816d50bd23bd5bbac4222d8c4fee66
NTeditor <nteditor4@gmail.com>
2025-08-10 12:37:11 +0300

    Исправлена обработка не root пользователя

--- README.md
+++ README.md
@@ -4,8 +4,11 @@
     - **.bashrc** или **.zshrc**: `alias tsu="$HOME/go/bin/go-tsu"`
     - **config.fish**: `alias tsu="$HOME/go/bin/go-tsu"`
 
+## dev
+1. Выполните команду: `go install github.com/NTeditor/go-tsu@dev`
+2. Добавьте alias:
+    - **.bashrc** или **.zshrc**: `alias tsu="$HOME/go/bin/go-tsu"`
+    - **config.fish**: `alias tsu="$HOME/go/bin/go-tsu"`
+
 # Проблемы
-
-
-
 - При закрытие **bash** может быть ошибка `exit status 1`
--- internal/env/shell.go
+++ internal/env/shell.go
@@ -11,28 +11,28 @@ import (
 
 func (e env) NewShell(shell string) *exec.Cmd {
 	envVars := strings.Join(e.genEnvVars(), " ")
-	cmd := exec.Command(e.suFile, "-i", "-c",
+	cmd := exec.Command(e.suFile, e.user, "-i", "-c",
 		fmt.Sprintf("%s env -i %s %s", e.getBusybox(), envVars, shell))
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	log.WithFields(log.Fields{
 		"envVars": envVars,
-		"cmd": cmd,
+		"cmd":     cmd,
 	}).Debugf("shell created")
 	return cmd
 }
 
 func (e env) NewCommand(shell string, command string) *exec.Cmd {
 	envVars := strings.Join(e.genEnvVars(), " ")
-	cmd := exec.Command(e.suFile, "-i", "-c",
+	cmd := exec.Command(e.suFile, e.user, "-i", "-c",
 		fmt.Sprintf("%s env -i %s %s -c %s", e.getBusybox(), envVars, shell, command))
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	log.WithFields(log.Fields{
 		"envVars": envVars,
-		"cmd": cmd,
+		"cmd":     cmd,
 	}).Debugf("command created")
 	return cmd
 }
